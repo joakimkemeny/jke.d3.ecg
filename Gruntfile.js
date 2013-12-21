7c18c5ed99d6c7eabc14f22cfd17de8ac7a52286
Joakim Kemeny <joakim@kemeny.se>
2013-12-22 00:08:10 +0100

    Code cleanup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,10 +107,10 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-gh-pages');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-gh-pages');
 
 	// Aliases
 	grunt.registerTask('build', ['jshint', 'compass', 'uglify', 'copy']);
